e276a71543a601a55c0d3f2e9b18b1173360d443
nanairo-develop <nanairo.develop@gmail.com>
2023-06-07 15:44:17 +0900

    fadeIn、fadeOut組み込み

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WebPage
+
+ウェブページのソース管理
+
+javascript の fadeIn／fadeOut は
+https://github.com/clovisdasilvaneto/fadein-fadeout-com-javascript-puro.git
+を参考にしました
--- docs/js/topbutton.js
+++ docs/js/topbutton.js
@@ -8,11 +8,44 @@ document.addEventListener("DOMContentLoaded", function () {
       window.setTimeout(me, 10);
     }
   });
-  //window.addEventListener("scroll", function () {
-  //  if (this.scrollTop() >= 200) {
-  //    pageTop.fadeIn();
-  //  } else {
-  //    pageTop.fadeOut();
-  //  }
-  //});
+  window.addEventListener("scroll", function () {
+    if (this.scrollY >= 200) {
+      fadeIn(pageTop, 1);
+    } else {
+      fadeOut(pageTop, 1);
+    }
+  });
 });
+
+// fadeIn fadeutロジック
+function fadeIn(element, time) {
+  inOUtAnimation(element, time, 0, 100);
+}
+
+function fadeOut(element, time) {
+  inOUtAnimation(element, time, 100, 0);
+}
+
+function inOUtAnimation(element, time, initial, end) {
+  if (initial == 0) {
+    increment = 2;
+    element.style.display = "block";
+  } else {
+    increment = -2;
+  }
+
+  opc = initial;
+
+  intervalo = setInterval(function () {
+    if (opc == end) {
+      if (end == 0) {
+        element.style.display = "none";
+      }
+      clearInterval(intervalo);
+    } else {
+      opc += increment;
+      element.style.opacity = opc / 100;
+      element.style.filter = "alpha(opacity=" + opc + ")";
+    }
+  }, time * 10);
+}
